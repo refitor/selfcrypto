91ff88830cc20503fb3f7e6399a90b283c51f45c
refitor <refitor@126.com>
2022-12-30 01:32:49 +0800

    update contract

--- contracts/selfcrypto.sol
+++ contracts/selfcrypto.sol
@@ -13,15 +13,19 @@ contract SelfData is Ownable {
     using ECDSA for bytes32;
     using SafeMath for uint256;
     using Address for address payable;
-    address private _wallet;
-    address private _nftAddr;
-    uint256 private _storeFee;
-    uint256 private _feeRate;
+    struct MetaData {
+        address wallet;
+        address nftAddr;
+        uint256 storeFee;
+        uint256 feeRate;
+        uint256 registTotal;
+    }
     struct StoreData {
         bytes recoverID;
         bytes web3Key;
         bytes backendKey;
     }
+    MetaData private _metaData;
     mapping (address => StoreData) private _dataMap;
 
     /**
@@ -47,14 +51,14 @@ contract SelfData is Ownable {
         require(web3Key.length != 0, "web3Key must be non-empty");
         require(recoverID.length != 0, "recoverID must be non-empty");
         require(backendKey.length != 0, "backendKey must be non-empty");
-        (address wallet, address nftAddr, uint256 storeFee, uint256 feeRate) = _get();
-        if (nftAddr != address(0)) {
-            require(IERC721(nftAddr).balanceOf(msg.sender) != 0, "illegal nft holder");
-        } else if (storeFee > 0 && feeRate > 0) {
+        MetaData memory md = _get();
+        if (md.nftAddr != address(0)) {
+            require(IERC721(md.nftAddr).balanceOf(msg.sender) != 0, "illegal nft holder");
+        } else if (md.storeFee > 0 && md.feeRate > 0) {
             require(msg.value > 0, "store fee must be greater than 0");
-            require(msg.value == storeFee, "invalid registration fee");
-            require(msg.value * feeRate / 10000 > 0, "invalid registration fee");
-            payable(wallet).sendValue(msg.value * feeRate / 10000);
+            require(msg.value == md.storeFee, "invalid registration fee");
+            require(msg.value * md.feeRate / 10000 > 0, "invalid registration fee");
+            payable(md.wallet).sendValue(msg.value * md.feeRate / 10000);
         }
         sd = StoreData(recoverID, web3Key, backendKey);
         _setKV(msg.sender, sd);
@@ -73,6 +77,14 @@ contract SelfData is Ownable {
     }
 
     /**
+     * @dev function load register total.
+     */
+    function Total() view external returns (uint256 registTotal) {
+        MetaData memory md = _get();
+        return md.registTotal;
+    }
+
+    /**
      * @dev function verify signature.
      * @param signature signature need verify.
      * @param message message need verify.
@@ -88,6 +100,7 @@ contract SelfData is Ownable {
      */
     function _setKV(address k, StoreData memory v) private {
         _dataMap[k] = v;
+        _metaData.registTotal = _metaData.registTotal + 1;
     }
 
     /**
@@ -106,19 +119,17 @@ contract SelfData is Ownable {
      * @param feeRate fee deduction percentage set by the contract creator, demo: 500 / 10000, feeRate is 500.
      */
     function _set(address wallet, address nftAddr, uint256 storeFee, uint256 feeRate) private {
-        _wallet = wallet;
+        _metaData = MetaData(wallet, nftAddr, 0, 0, 0);
         if (nftAddr == address(0)) {
-            _storeFee = storeFee;
-            _feeRate = feeRate;
-        } else {
-            _nftAddr = nftAddr;
+            _metaData.storeFee = storeFee;
+            _metaData.feeRate = feeRate;
         }
     }
 
     /**
      * @dev function get private variable.
      */
-    function _get() private view returns (address, address, uint256, uint256) {
-        return (_wallet, _nftAddr, _storeFee, _feeRate);
+    function _get() private view returns (MetaData memory md) {
+        return _metaData;
     }
 }
